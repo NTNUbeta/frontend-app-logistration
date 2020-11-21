a6394433539635a244235d0d1f343e1b89e93c9f
Adeel Khan <muhammad.adeel@arbisoft.com>
2020-11-21 06:49:54 +0500

    update.

--- src/logistration/tests/RegistrationPage.test.jsx
+++ src/logistration/tests/RegistrationPage.test.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
 import configureStore from 'redux-mock-store';
 import { IntlProvider, injectIntl, configure } from '@edx/frontend-platform/i18n';
 
@@ -47,6 +48,105 @@ describe('./RegistrationPage.js', () => {
 
   afterEach(() => {
     jest.clearAllMocks();
+  });
+
+  it('should show error message on invalid email', () => {
+    const validationMessage = 'Enter a valid email address that contains at least 3 characters.';
+    store = mockStore({
+      ...store,
+      logistration: {
+        ...store.logistration,
+        registrationResult: {
+          success: false,
+          redirectUrl: '',
+        },
+        registrationError: null,
+        formData: {
+          fields: [{
+            label: 'I agree to the Your Platform Name Here <a href="/honor" rel="noopener" target="_blank">Honor Code</a>',
+            name: 'honor_code',
+            type: 'checkbox',
+            errorMessages: {
+              required: 'You must agree to the Your Platform Name Here Honor Code',
+            },
+          },
+          {
+            label: 'Gender',
+            name: 'gender',
+            type: 'select',
+            errorMessages: {
+              required: '',
+            },
+          },
+          {
+            label: 'Mailing address',
+            name: 'mailing_address',
+            type: 'textarea',
+            errorMessages: {
+              required: 'Enter your mailing address.',
+            },
+          }],
+        },
+      },
+    });
+
+    const registrationPage = mount(reduxWrapper(<IntlRegistrationPage {...props} />));
+    registrationPage.find('input#email').simulate('blur', { target: { name: '' } });
+    registrationPage.update();
+    expect(registrationPage.find('#email-invalid-feedback').text()).toEqual(validationMessage);
+  });
+
+  it('should show error message on invalid username', () => {
+    const validationMessage = 'Username must be between 2 and 30 characters long.';
+    store = mockStore({
+      ...store,
+      logistration: {
+        ...store.logistration,
+        registrationResult: {
+          success: false,
+          redirectUrl: '',
+        },
+        registrationError: null,
+        formData: {
+          fields: [{
+            label: 'I agree to the Your Platform Name Here <a href="/honor" rel="noopener" target="_blank">Honor Code</a>',
+            name: 'honor_code',
+            type: 'checkbox',
+            errorMessages: {
+              required: 'You must agree to the Your Platform Name Here Honor Code',
+            },
+          },
+          {
+            label: 'Gender',
+            name: 'gender',
+            type: 'select',
+            errorMessages: {
+              required: '',
+            },
+          },
+          {
+            label: 'Mailing address',
+            name: 'mailing_address',
+            type: 'textarea',
+            errorMessages: {
+              required: 'Enter your mailing address.',
+            },
+          }],
+        },
+      },
+    });
+
+    const registrationPage = mount(reduxWrapper(<IntlRegistrationPage {...props} />));
+    registrationPage.find('input#username').simulate('blur', { target: { name: '' } });
+    registrationPage.update();
+    expect(registrationPage.find('#username-invalid-feedback').text()).toEqual(validationMessage);
+  });
+
+  it('should call the componentDidMount lifecycle method', () => {
+    const spy = jest.spyOn(RegistrationPage.WrappedComponent.prototype, 'componentDidMount');
+
+    mount(reduxWrapper(<IntlRegistrationPage {...props} />));
+    expect(spy).toHaveBeenCalled();
   });
 
   it('should match default section snapshot', () => {
@@ -126,10 +226,37 @@ describe('./RegistrationPage.js', () => {
             },
           ],
         },
+        formData: {
+          fields: [{
+            label: 'I agree to the Your Platform Name Here <a href="/honor" rel="noopener" target="_blank">Honor Code</a>',
+            name: 'honor_code',
+            type: 'checkbox',
+            errorMessages: {
+              required: 'You must agree to the Your Platform Name Here Honor Code',
+            },
+          },
+          {
+            label: 'Gender',
+            name: 'gender',
+            type: 'select',
+            errorMessages: {
+              required: '',
+            },
+          },
+          {
+            label: 'Mailing address',
+            name: 'mailing_address',
+            type: 'textarea',
+            errorMessages: {
+              required: 'Enter your mailing address.',
+            },
+          }],
+        },
       },
     });
 
     const tree = renderer.create(reduxWrapper(<IntlRegistrationPage {...props} />)).toJSON();
     expect(tree).toMatchSnapshot();
+    windowSpy.mockClear();
   });
 });
